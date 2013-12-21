e964f7e4a2b625d2e098f04bf0d22b845f512f60
Rob Stinogle <stinogle@gmail.com>
2013-12-21 14:13:37 -0700

    Initial setup of Angular Phonegap project.

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// TODO: remove any unused injections
+angular.module('phraseApp', [
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('phraseApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
