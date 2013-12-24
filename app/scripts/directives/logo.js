a93c4d5f9ac26d86572485271c666a00f3b61413
Rob Stinogle <stinogle@gmail.com>
2013-12-24 12:27:28 -0700

    TODOs for logo directive.

--- app/scripts/directives/logo.js
+++ app/scripts/directives/logo.js
@@ -2,6 +2,9 @@
 
 angular.module('phraseApp')
   .directive('logo', function () {
+    // TODO: Read on dependency injection in directives
+    // How do we tie these to certain controllers?
+    // Why is scope injected without the $ sign?
     return {
       template: '<h1><a href="#/">logoText()</a></h1>',
       restrict: 'E',
