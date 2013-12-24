593d41e60b47f44aa1d2495e7b547cc7d52f271d
Rob Stinogle <stinogle@gmail.com>
2013-12-24 12:37:13 -0700

    Documentation.

--- app/scripts/services/filereader.js
+++ app/scripts/services/filereader.js
@@ -2,15 +2,47 @@
 
 angular.module('PhoneGap')
   .factory('FileReader', function () {
-    // Service logic
-    // ...
 
-    var meaningOfLife = 42;
+    // device APIs are available
+    //
+    // function onDeviceReady() {
+    //     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    //     console.log('Device ready callback');
+    // }
 
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
+    // function gotFS(fileSystem) {
+    //     fileSystem.root.getFile('readme.txt', null, gotFileEntry, fail);
+    // }
+
+    // function gotFileEntry(fileEntry) {
+    //     fileEntry.file(gotFile, fail);
+    // }
+
+    // function gotFile(file){
+    //     readDataUrl(file);
+    //     readAsText(file);
+    // }
+
+    // function readDataUrl(file) {
+    //     var reader = new FileReader();
+    //     reader.onloadend = function(evt) {
+    //         console.log('Read as data URL');
+    //         console.log(evt.target.result);
+    //     };
+    //     reader.readAsDataURL(file);
+    // }
+
+    // function readAsText(file) {
+    //     var reader = new FileReader();
+    //     reader.onloadend = function(evt) {
+    //         console.log('Read as text');
+    //         console.log(evt.target.result);
+    //     };
+    //     reader.readAsText(file);
+    // }
+
+    // function fail(error) {
+    //     console.log(error.code);
+    // }
+
   });
--- app/scripts/services/words.js
+++ app/scripts/services/words.js
@@ -4,14 +4,18 @@ angular.module('phraseApp')
   .factory('words', function ( $http, settings ) {
     var words = [],
         usedWords = JSON.parse(sessionStorage.getItem('usedWords') || 'null') || {},
-        wordListId = wordListSetting(),
         apiPath = 'http://www.pop-phrase.com/api/words/';
 
+    // Grab the selected category, and setup a slug for the name
     function wordListSetting() {
       return settings.get('Category').toLowerCase().replace(/[^a-z_]/g, '_');
     }
 
+    // TODO: this can probably have the function attached directly to the variable
+    var wordListId = wordListSetting();
+
     function onlyUnused( word ) {
+      // If the word is in the usedWords array, do not return it
       return word && !usedWords[word];
     }
 
@@ -61,6 +65,7 @@ angular.module('phraseApp')
         // No more words! Reset for now!
         usedWords = {};
         // TODO: Let's just reset the list here
+        // TODO: research messaging system provided with PhoneGap
         alert('You\'ve used all the words! Please choose another category.');
       }
       var word = unused[Math.floor(Math.random() * unused.length)];
@@ -73,50 +78,6 @@ angular.module('phraseApp')
         console.log('Word undefined! Why?');
       }
     }
-
-
-    // device APIs are available
-    //
-    // function onDeviceReady() {
-    //     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-    //     console.log('Device ready callback');
-    // }
-
-    // function gotFS(fileSystem) {
-    //     fileSystem.root.getFile('readme.txt', null, gotFileEntry, fail);
-    // }
-
-    // function gotFileEntry(fileEntry) {
-    //     fileEntry.file(gotFile, fail);
-    // }
-
-    // function gotFile(file){
-    //     readDataUrl(file);
-    //     readAsText(file);
-    // }
-
-    // function readDataUrl(file) {
-    //     var reader = new FileReader();
-    //     reader.onloadend = function(evt) {
-    //         console.log('Read as data URL');
-    //         console.log(evt.target.result);
-    //     };
-    //     reader.readAsDataURL(file);
-    // }
-
-    // function readAsText(file) {
-    //     var reader = new FileReader();
-    //     reader.onloadend = function(evt) {
-    //         console.log('Read as text');
-    //         console.log(evt.target.result);
-    //     };
-    //     reader.readAsText(file);
-    // }
-
-    // function fail(error) {
-    //     console.log(error.code);
-    // }
-
 
     return {
       all: ensureFetched(allWords),
