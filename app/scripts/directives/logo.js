2b5a3ac9d53cf570d716cc5ee240ba6894a6cc88
Rob Stinogle <stinogle@gmail.com>
2013-12-26 08:37:23 -0700

    Logo directive link.

--- app/scripts/directives/logo.js
+++ app/scripts/directives/logo.js
@@ -5,9 +5,14 @@ angular.module('phraseApp')
     // TODO: Read on dependency injection in directives
     // How do we tie these to certain controllers?
     // Why is scope injected without the $ sign?
+    var logoState = function ( scope, element ) {
+      // body...
+    };
+
     return {
       template: '<h1><a href="#/">logoText()</a></h1>',
       restrict: 'E',
+      link: logoState,
       scope: {}
     };
 
