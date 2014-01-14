cb095a692c9dd3ab7395aa06248cace452b4c522
Rob Stinogle <stinogle@gmail.com>
2014-01-14 16:06:03 -0700

    Filereader start.

--- app/scripts/services/filereader.js
+++ app/scripts/services/filereader.js
@@ -7,48 +7,46 @@ angular.module('PhoneGap')
 
       PhoneGap.ready().then(function () {
         console.log('FILE READER');
+
+        // device APIs are available
+
+        // window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+
+        // function gotFS(fileSystem) {
+        //     fileSystem.root.getFile('readme.txt', null, gotFileEntry, fail);
+        // }
+
+        // function gotFileEntry(fileEntry) {
+        //     fileEntry.file(gotFile, fail);
+        // }
+
+        // function gotFile(file){
+        //     readDataUrl(file);
+        //     readAsText(file);
+        // }
+
+        // function readDataUrl(file) {
+        //     var reader = new FileReader();
+        //     reader.onloadend = function(evt) {
+        //         console.log('Read as data URL');
+        //         console.log(evt.target.result);
+        //     };
+        //     reader.readAsDataURL(file);
+        // }
+
+        // function readAsText(file) {
+        //     var reader = new FileReader();
+        //     reader.onloadend = function(evt) {
+        //         console.log('Read as text');
+        //         console.log(evt.target.result);
+        //     };
+        //     reader.readAsText(file);
+        // }
+
+        // function fail(error) {
+        //     console.log(error.code);
+        // }
       });
-      // device APIs are available
-      //
-      // function onDeviceReady() {
-      //     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-      //     console.log('Device ready callback');
-      // }
-
-      // function gotFS(fileSystem) {
-      //     fileSystem.root.getFile('readme.txt', null, gotFileEntry, fail);
-      // }
-
-      // function gotFileEntry(fileEntry) {
-      //     fileEntry.file(gotFile, fail);
-      // }
-
-      // function gotFile(file){
-      //     readDataUrl(file);
-      //     readAsText(file);
-      // }
-
-      // function readDataUrl(file) {
-      //     var reader = new FileReader();
-      //     reader.onloadend = function(evt) {
-      //         console.log('Read as data URL');
-      //         console.log(evt.target.result);
-      //     };
-      //     reader.readAsDataURL(file);
-      // }
-
-      // function readAsText(file) {
-      //     var reader = new FileReader();
-      //     reader.onloadend = function(evt) {
-      //         console.log('Read as text');
-      //         console.log(evt.target.result);
-      //     };
-      //     reader.readAsText(file);
-      // }
-
-      // function fail(error) {
-      //     console.log(error.code);
-      // }
 
     }
   ]);
