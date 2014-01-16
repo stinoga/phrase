6b97eca6618add64adc4e68f02be6507e4bc7d23
Rob Stinogle <stinogle@gmail.com>
2014-01-15 19:08:47 -0700

    Starting fileReader service and removing jshint from grunt.

--- app/scripts/services/filereader.js
+++ app/scripts/services/filereader.js
@@ -2,53 +2,58 @@
 
 angular.module('PhoneGap')
   .factory('FileReader', [
+    '$window',
     'PhoneGap',
-    function (PhoneGap) {
+    function ($window, PhoneGap) {
 
       PhoneGap.ready().then(function () {
         console.log('FILE READER');
       });
-      // device APIs are available
-      //
-      // function onDeviceReady() {
-      //     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-      //     console.log('Device ready callback');
-      // }
 
-      // function gotFS(fileSystem) {
-      //     fileSystem.root.getFile('readme.txt', null, gotFileEntry, fail);
-      // }
+      return {
+        function onDeviceReady() {
+          PhoneGap.ready().then(function () {
+            $window.navigator.compass.getCurrentHeading(onSuccess, onError, options);
+          });
+          $window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+          console.log('Device ready callback');
+        }
 
-      // function gotFileEntry(fileEntry) {
-      //     fileEntry.file(gotFile, fail);
-      // }
+        function gotFS(fileSystem) {
+          fileSystem.root.getFile('readme.txt', null, gotFileEntry, fail);
+        }
 
-      // function gotFile(file){
-      //     readDataUrl(file);
-      //     readAsText(file);
-      // }
+        function gotFileEntry(fileEntry) {
+          fileEntry.file(gotFile, fail);
+        }
 
-      // function readDataUrl(file) {
-      //     var reader = new FileReader();
-      //     reader.onloadend = function(evt) {
-      //         console.log('Read as data URL');
-      //         console.log(evt.target.result);
-      //     };
-      //     reader.readAsDataURL(file);
-      // }
+        function gotFile(file){
+          readDataUrl(file);
+          readAsText(file);
+        }
 
-      // function readAsText(file) {
-      //     var reader = new FileReader();
-      //     reader.onloadend = function(evt) {
-      //         console.log('Read as text');
-      //         console.log(evt.target.result);
-      //     };
-      //     reader.readAsText(file);
-      // }
+        function readDataUrl(file) {
+          var reader = new FileReader();
+          reader.onloadend = function(evt) {
+            console.log('Read as data URL');
+            console.log(evt.target.result);
+          };
+          reader.readAsDataURL(file);
+        }
 
-      // function fail(error) {
-      //     console.log(error.code);
-      // }
+        function readAsText(file) {
+          var reader = new FileReader();
+          reader.onloadend = function(evt) {
+            console.log('Read as text');
+            console.log(evt.target.result);
+          };
+          reader.readAsText(file);
+        }
+
+        function fail(error) {
+          console.log(error.code);
+        }
+      };
 
     }
   ]);
