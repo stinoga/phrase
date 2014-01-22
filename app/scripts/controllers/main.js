4b311addeda80e9b469ed79f2f1833f9c76221f4
Rob Stinogle <stinogle@gmail.com>
2014-01-21 22:17:20 -0700

    Initial code for sound service.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('phraseApp')
-  .controller('MainCtrl', function ($scope, timer, settings) {
+  .controller('MainCtrl', function ($scope, timer, settings, sound) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -13,6 +13,10 @@ angular.module('phraseApp')
     $scope.Category = settings.get('Category').id;
     $scope.Timer = settings.get('Timer').id;
 
+    if (settings.get('Timer').name === 'Audible') {
+      sound.play('test');
+    }
+
     $scope.startTimer = function() {
 
       // WORKS ON TABLET, BUT SUBSEQUENT TIMER CALL DOESN'T PLAY AUDIO
@@ -22,10 +26,6 @@ angular.module('phraseApp')
       //         'audio/beep.ogg'
       //       ]
       //     });
-
-      if (settings.get('Timer').name === 'Audible') {
-        // beep.play();
-      }
 
       // TODO: DOESN'T WORK ON TABLET. POSSIBLY SOMETHING WITH TIMER FUNCTION?
       // function round_over() {
--- app/scripts/services/sound.js
+++ app/scripts/services/sound.js
@@ -1,9 +1,9 @@
 'use strict';
 
-angular.module('phraseApp')
-  .factory('sound', function ( $window) {
+angular.module('PhoneGap')
+  .factory('sound', function ( $window, PhoneGap ) {
 
-    var soundOk = false;
+    // var soundOk = false;
 
     // function checkSound() {
     //   if (soundOk) {
@@ -29,5 +29,86 @@ angular.module('phraseApp')
 
     // $window.addEventListener('touchstart', checkSound, false);
     // $window.addEventListener('click', checkSound, false);
+
+    // Audio player
+    //
+    var myMedia = null,
+        mediaTimer = null;
+
+    // Play audio
+    //
+    function playAudio(src) {
+      PhoneGap.ready().then(function () {
+        console.log('play');
+        if (myMedia === null) {
+          // Create Media object from src
+          myMedia = new Media(src, onSuccess, onError);
+        } // else play current audio
+        // Play audio
+        myMedia.play();
+
+        // Update myMedia position every second
+        if (mediaTimer === null) {
+          mediaTimer = setInterval(function() {
+            // get myMedia position
+            myMedia.getCurrentPosition(
+              // success callback
+              function(position) {
+                if (position > -1) {
+                  setAudioPosition((position) + ' sec');
+                }
+              },
+              // error callback
+              function(e) {
+                console.log('Error getting pos=' + e);
+                setAudioPosition('Error: ' + e);
+              }
+            );
+          }, 1000);
+        }
+      });
+    }
+
+    // Pause audio
+    //
+    function pauseAudio() {
+      if (myMedia) {
+        myMedia.pause();
+      }
+    }
+
+    // Stop audio
+    //
+    function stopAudio() {
+      if (myMedia) {
+        myMedia.stop();
+      }
+      clearInterval(mediaTimer);
+      mediaTimer = null;
+    }
+
+    // onSuccess Callback
+    //
+    function onSuccess() {
+      console.log('playAudio():Audio Success');
+    }
+
+    // onError Callback
+    //
+    function onError(error) {
+      alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+    }
+
+    // Set audio position
+    //
+    function setAudioPosition(position) {
+      document.getElementById('audio_position').innerHTML = position;
+    }
+
+    return {
+      play: playAudio,
+      pause: pauseAudio,
+      stop: stopAudio
+    };
 
   });
