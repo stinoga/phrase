a50b74038eb5a4dd9572c01678fe9b50812608fe
Rob Stinogle <stinogle@gmail.com>
2014-03-23 23:02:42 -0600

    Continue game option and score clearing service.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,27 @@
 'use strict';
 
 angular.module('phraseApp')
-  .controller('MainCtrl', function ($rootScope, $scope, settings, page) {
+  .controller('MainCtrl', function ($rootScope, $scope, $location, settings, page, score) {
     $rootScope.pageName = page.get();
+
+    $scope.teams = score.all();
+    $scope.continueGame = false;
+
+    $scope.newGame = function() {
+      score.clear();
+      $location.path('/play');
+    };
+
+    // Watch for score changes
+    // If our score is above zero, let's show the continue game button
+    $scope.$watch(function () {
+      var totalScore = parseInt($scope.teams[0].score) + parseInt($scope.teams[1].score);
+      if (totalScore > 0) {
+        $scope.continueGame = true;
+      } else {
+        $scope.continueGame = false;
+      }
+    });
 
     // These will handle storing our default setting values if they
     // haven't been stored in localStorage yet
